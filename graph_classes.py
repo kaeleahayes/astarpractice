87b27af88011fb2f2a9e061c642e7e7d255e6f11
kaeleahayes <kaeleahayes@gmail.com>
2025-06-30 15:37:17 -0400

    Create graph_classes.py

---
+++ graph_classes.py
@@ -0,0 +1,17 @@
+
+class Polytope(self):
+    self.vertices = {}
+    self.xv = {}
+    self.yv = {}
+    self.distances = {}
+    self.mean = {}
+    self.area = {}
+    self.max_radius = {}
+    self.min_radius = {}
+    self.mean_radius = {}
+    self.radii = {}
+    self.cost = {}
+
+class Vertex(self):
+    
+class Edge(self):
